f4cf3fbc1b4abc56cec1e07b1ba517b86b644e7c
Patrik Silfverberg <b20patsi@student.his.se>
2021-04-19 10:11:57 +0200

    Added finish function on second button

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,6 +3,9 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button secondButton = findViewById(R.id.second_button);
+
+        secondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("BACK_BUTTON", "Back to main activity");
+
+                finish();
+            }
+        });
     }
 }
